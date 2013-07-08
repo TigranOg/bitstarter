18603b66c5a9debea7da1ef57619b4ec1e65795e
TigranOg <tigagalaxy@gmail.com>
2013-07-08 18:53:56 +0000

    output from html

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var buf = new Buffer(k);
 
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+  response.send('sdfsdfsdf');
 });
 
 var port = process.env.PORT || 5000;
